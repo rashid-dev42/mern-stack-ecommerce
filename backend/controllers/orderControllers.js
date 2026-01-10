dfc6efbde5679f2184806f89927c006e2ff407b8
devCake1 <shipc4836@gmail.com>
2026-01-11 02:01:21 +0600

    fix: remove forward slash from url to prevent 404

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -77,7 +77,7 @@ const verifyCheckout = async (req, res, next) => {
           * If the order has already confirmed, prevent re-verification of this order
           * Otherwise, in-stock item count of each products of this order will be reduced in the database
           */
-         return res.redirect(`https://gilded-cassata-4e9f28.netlify.app//verify-order?order_id=${orderId}`);
+         return res.redirect(`https://gilded-cassata-4e9f28.netlify.app/verify-order?order_id=${orderId}`);
       }
       order.cartItems.forEach(async (cartItem) => {
         const product = await Product.findOne({ _id: cartItem.productId });
