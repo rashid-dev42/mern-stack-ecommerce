2e8dec7b912a78cf7a9353c6a2786b225d10c5d5
devCake1 <shipc4836@gmail.com>
2026-01-12 19:51:24 +0600

    fix: prevent image loading from localhost

--- frontend/src/components/singleProduct/SingleProduct.jsx
+++ frontend/src/components/singleProduct/SingleProduct.jsx
@@ -10,7 +10,7 @@ const SingleProduct = ({ product }) => {
     <div className="SingleProduct">
       <div className="SingleProduct-img">
         {product.imgPath === "" && <img src={imageNotFound} alt=""/>}
-        {product.imgPath !== "" && <img src={"http://localhost:5001/" + product.imgPath} alt=""/>}
+        {product.imgPath !== "" && <img src={import.meta.env.VITE_API_BASE_URL + "/" + product.imgPath} alt=""/>}
       </div>
       <h4>{product.productName}</h4>
       <h5>${product.price}</h5>
